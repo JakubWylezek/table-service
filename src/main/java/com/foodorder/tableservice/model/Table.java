3bd6d0d7891c1214393899dd7592f1423a8c368a
jakub.wylezek <jakub.wylezek@wp.pl>
2019-07-16 13:20:42 +0200

    Created Table model

---
+++ src/main/java/com/foodorder/tableservice/model/Table.java
@@ -0,0 +1,13 @@
+package com.foodorder.tableservice.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Table {
+
+    private String id;
+    private int number;
+
+}
